dca03e805349c2a8a0536508c22c9cc3fbb707fe
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-03-06 23:32:33 -0500

    minor styling update for desktop view of job board (unfiltered)

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
                     </div>
                     <div className="absolute bottom-0 left-0 flex h-[228px] w-[350px] flex-col rounded-md bg-white px-8 pt-[49px] text-gray-300">
                         <div className="flex items-center">
-                            {job.postedAt}{' '}
-                            <span className="mx-3 h-1 w-1 rounded-full bg-gray-300"></span>{' '}
+                            {job.postedAt}
+                            <span className="mx-3 h-1 w-1 rounded-full bg-gray-300" />
                             {job.contract}
                         </div>
                         <h3 className="mt-3 text-blue-100">{job.position}</h3>
