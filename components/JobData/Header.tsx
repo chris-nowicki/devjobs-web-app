bfb5b69d3709dedc2273f9f7f6d0e124e031bb93
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-03-09 12:44:26 -0500

    worked on styling of job description page

--- components/JobData/Header.tsx
+++ components/JobData/Header.tsx
@@ -8,7 +8,7 @@ import { BGHeaderDesktop, BGHeaderTablet, BGHeaderMobile, Logo } from '../Icons'
 
 export default function Header({ data }: { data: any }) {
     return (
-        <div className="relative flex h-[202px] w-full flex-col items-center">
+        <div className="relative flex h-[326px] w-full flex-col items-center md:h-[261px]">
             <div className="-z-10">
                 <div className="absolute top-0 left-0 hidden lg:block">
                     <BGHeaderDesktop />
@@ -26,9 +26,9 @@ export default function Header({ data }: { data: any }) {
                     <ThemeSwitcher />
                 </div>
             </div>
-            <div className="absolute -bottom-[41px] flex h-[140px] w-full max-w-[327px] items-center rounded-bl-md rounded-br-md rounded-tr-md  bg-white dark:bg-blue-100 md:max-w-[689px]">
+            <div className="absolute bottom-0 flex h-full max-h-[205px] w-full max-w-[327px] flex-col items-center rounded-md bg-white dark:bg-blue-100 md:max-h-[140px] md:max-w-[689px] md:flex-row  md:rounded-bl-md md:rounded-br-md md:rounded-tr-md md:rounded-tl-none lg:max-w-[730px]">
                 <div
-                    className="flex h-[140px] w-[140px] items-center justify-center rounded-bl-md"
+                    className="flex h-[50px] w-[50px] -translate-y-1/2 items-center justify-center rounded-[15px] md:h-[140px] md:w-[140px] md:-translate-y-0 md:-translate-x-0 md:rounded-bl-md md:rounded-tl-none md:rounded-tr-none md:rounded-br-none"
                     style={{ backgroundColor: data.logoBackground }}
                 >
                     <Image
@@ -36,14 +36,25 @@ export default function Header({ data }: { data: any }) {
                         height={23}
                         src={data.logo}
                         alt={data.company}
+                        className="hidden md:block"
+                    />
+                    <Image
+                        width={40}
+                        height={12}
+                        src={data.logo}
+                        alt={data.company}
+                        className="block md:hidden"
                     />
                 </div>
-                <div className="flex w-[590px] items-center justify-between pl-10 pr-[43px]">
-                    <div className="flex h-[56px] w-[78px] flex-col gap-[13px]">
+                <div className="flex w-[590px] flex-col items-center justify-between pl-10 pr-[43px] md:flex-row">
+                    <div className="flex h-[56px] w-[78px] flex-col items-center gap-[13px] md:items-start">
                         <h2>{data.company}</h2>
-                        <span>{data.website}</span>
+                        <span className="text-gray-300">{data.website}</span>
                     </div>
-                    <Link href={data.website} className="bg-[#5964E0] bg-opacity-10 hover:bg-opacity-[.35] rounded-[5px] px-5 py-4 text-[#5964E0] font-bold text-[16px]">
+                    <Link
+                        href={data.website}
+                        className="mt-6 flex h-12 w-[147px] items-center justify-center rounded-[5px] bg-[#5964E0] bg-opacity-10 text-[16px] font-bold text-[#5964E0] hover:bg-opacity-[.35] dark:bg-white dark:bg-opacity-10 dark:text-white dark:hover:bg-opacity-[.35] md:mt-0"
+                    >
                         Company Site
                     </Link>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,8 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
